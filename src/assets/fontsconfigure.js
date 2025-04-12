e018be7483b5e7357c11d7a04dcfec5f33cb2953
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2025-04-13 00:15:52 +0800

    font family configured

---
+++ src/assets/fontsconfigure.js
@@ -0,0 +1,22 @@
+import localFont from "next/font/local";
+export const NotoSans = localFont({
+    src:[
+      {
+        path:'../assets/fonts/static/NotoSans-Bold.ttf',
+        weight:'700',
+        style:'normal',
+      },
+      {
+        path:'../assets/fonts/static/NotoSans-Medium.ttf',
+        weight:'500',
+        style:'normal',
+      },
+      {
+        path:'../assets/fonts/static/NotoSans-Regular.ttf',
+        weight:'400',
+        style:'normal',
+      },
+    ],
+   
+     })
+  
